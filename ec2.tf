08bf49494a72b4812bb2681dcce0d975ea4f039b
Travis Fernandes <travisfernandes2327@gmail.com>
2026-02-15 12:40:16 +0530

    added correct configuration for ec2

--- ec2.tf
+++ ec2.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-# Subnet
+# Public Subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -26,6 +26,7 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
+# Private Subnet
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
@@ -80,9 +81,16 @@ resource "aws_route_table_association" "private_subnet_assoc" {
   route_table_id = aws_route_table.private_rt.id
 }
 
-# -------------------
+# Default Security Group
+resource "aws_default_security_group" "default-sg" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "my-default-sg"
+  }
+}
+
 # PUBLIC SECURITY GROUP
-# -------------------
 resource "aws_security_group" "public_sg" {
   name        = "public-sg"
   description = "security groups for public subnet"
@@ -129,45 +137,16 @@ resource "aws_vpc_security_group_egress_rule" "public_all_outbound" {
   description       = "Allow all outbound traffic"
 }
 
-
-# PRIVATE SECURITY GROUP
-resource "aws_security_group" "private_sg" {
-  name        = "private-sg"
-  description = "security group for private subnet"
-  vpc_id      = aws_vpc.main.id
-
-  tags = {
-    Name = "private-sg"
-  }
-}
-
-# Ingress rule
-resource "aws_vpc_security_group_ingress_rule" "private_from_public" {
-  security_group_id            = aws_security_group.private_sg.id
-  referenced_security_group_id = aws_security_group.public_sg.id
-  ip_protocol                  = "-1"
-  description                  = "Allow all traffic from public security group"
-}
-
-# Egress Rule
-resource "aws_vpc_security_group_egress_rule" "private_all_outbound" {
-  security_group_id = aws_security_group.private_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-  description       = "Allow all outbound traffic"
-}
-
-
 # EC2 INSTANCE
 resource "aws_instance" "my_instance" {
   key_name               = aws_key_pair.my_key.key_name
   vpc_security_group_ids = [aws_security_group.public_sg.id]
   subnet_id              = aws_subnet.public_subnet.id
   instance_type          = "t2.micro"
-  ami                    = "ami-0360c520857e3138f" # Ubuntu 24.04 LTS
+  ami                    = "ami-0b6c6ebed2801a5cb" # Ubuntu 24.04 LTS
 
   root_block_device {
-    volume_size = 15
+    volume_size = 12
     volume_type = "gp3"
   }
 
